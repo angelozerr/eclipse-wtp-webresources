5180bf673a093fc78b39bdad2ffeabdfc0ff5162
Kaloyan Raev <kaloyan.r@zend.com>
2015-06-26 10:20:26 +0300

    Fix #43: Avoid validating class values with PHP code inside

--- org.eclipse.a.wst.html.webresources.core/src/org/eclipse/wst/html/webresources/internal/core/validation/PHPScriptIgnoreValidator.java
+++ org.eclipse.a.wst.html.webresources.core/src/org/eclipse/wst/html/webresources/internal/core/validation/PHPScriptIgnoreValidator.java
@@ -12,15 +12,18 @@ package org.eclipse.wst.html.webresources.internal.core.validation;
 
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.content.IContentType;
+import org.eclipse.wst.html.core.internal.provisional.HTML40Namespace;
 import org.eclipse.wst.html.webresources.core.providers.IWebResourcesContext;
 import org.eclipse.wst.html.webresources.core.providers.WebResourceKind;
 import org.eclipse.wst.html.webresources.core.validation.IWebResourcesIgnoreValidator;
 import org.eclipse.wst.xml.core.internal.provisional.document.IDOMAttr;
 import org.eclipse.wst.xml.core.internal.provisional.document.IDOMNode;
 
-
-@SuppressWarnings( "restriction" )
+@SuppressWarnings("restriction")
 public class PHPScriptIgnoreValidator implements IWebResourcesIgnoreValidator {
+
+	private static final String START_SCRIPT = "<?php";
+	private static final String END_SCRIPT = "?>";
 
 	private final IContentType htmlContentType;
 	private final IContentType phpContentType;
@@ -37,22 +40,24 @@ public class PHPScriptIgnoreValidator implements IWebResourcesIgnoreValidator {
 
 			try {
 				fileContentType = context.getHtmlFile().getContentDescription().getContentType();
+			} catch (Exception e) {
 			}
-			catch( Exception e ) { }
 
-			if (fileContentType != null && 
-					(fileContentType.isKindOf(phpContentType) || fileContentType.isKindOf(htmlContentType))) {
+			if (fileContentType != null
+					&& (fileContentType.isKindOf(phpContentType) || fileContentType.isKindOf(htmlContentType))) {
 				final IDOMNode node = context.getHtmlNode();
 
 				if (node instanceof IDOMAttr) {
 					final IDOMAttr attr = (IDOMAttr) node;
 					final String attrName = attr.getName();
 
-					if (attrName != null &&
-						(attrName.equalsIgnoreCase("href") || attrName.equalsIgnoreCase("src"))) {
+					if (attrName != null && (HTML40Namespace.ATTR_NAME_HREF.equalsIgnoreCase(attrName)
+							|| HTML40Namespace.ATTR_NAME_SRC.equalsIgnoreCase(attrName)
+							|| HTML40Namespace.ATTR_NAME_CLASS.equalsIgnoreCase(attrName)
+							|| HTML40Namespace.ATTR_NAME_ID.equalsIgnoreCase(attrName))) {
 						final String val = attr.getValue();
 
-						if (val != null && (val.contains("<?php") || val.contains("?>"))) {
+						if (val != null && (val.contains(START_SCRIPT) || val.contains(END_SCRIPT))) {
 							return true;
 						}
 					}
